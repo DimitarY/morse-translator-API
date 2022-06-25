d66943c051b2e013d93a0a355598e0eeaacbf21e
DimitarY <dimitar.yordanov11@outlook.com>
2022-06-25 17:54:27 +0300

    Creating POST request for /download/json_files
    This request accept languages and return them as an archive

--- JSON.py
+++ JSON.py
@@ -1,4 +1,5 @@
-from flask_restful import Resource
+from zipfile import ZipFile
+from flask_restful import Resource, reqparse
 from flask import send_file
 
 import glob
@@ -16,13 +17,36 @@ class Languages(Resource):
 
         return languages, 200
 
+
 class Files(Resource):
     def get(self):
-        filename = "json.zip"
+        archiveName = "languages.zip"
+        returnName = "languages.zip"
         path = "archives/"
-        
+
         try:
-            if os.path.getmtime("JSON") <= os.path.getmtime(filename):
-                return send_file(path + filename, attachment_filename=filename, as_attachment=True)
+            if os.path.getmtime("JSON") <= os.path.getmtime(archiveName):
+                return send_file(path + archiveName, attachment_filename=returnName, as_attachment=True)
         except FileNotFoundError:
-            return send_file(shutil.make_archive(path + filename[:-4], "zip", "JSON"), attachment_filename=filename, as_attachment=True)
+            return send_file(shutil.make_archive(path + archiveName[:-4], "zip", "JSON"), attachment_filename=returnName, as_attachment=True)
+
+    def post(self):
+        archiveName = "requiredLanguages.zip"
+        returnName = "languages.zip"
+        path = "archives/"
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('languages', type=str,
+                            action="append", location="json", required=True)
+        args = parser.parse_args()
+
+        with ZipFile(path + archiveName, "w") as zipObj:
+            os.chdir("JSON")
+            zipObj.write("Numbers.json")
+            zipObj.write("Symbols.json")
+            for x in args["languages"]:
+                if (os.path.exists(x + ".json")):
+                    zipObj.write(x + ".json")
+            os.chdir("..")
+
+        return send_file(path + archiveName, attachment_filename=returnName, as_attachment=True)
