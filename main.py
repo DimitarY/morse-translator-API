55ff0a866b9ab2eced99cc5d12b3292cb591c8ea
DimitarY <dimitar.yordanov11@outlook.com>
2022-06-26 18:32:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Api, Resource
 
 from JSON import Languages, Files
 from setupInfo import SetupVersion, DownloadSetup
@@ -9,6 +9,16 @@ from updateSetupInfo import updateSetupData
 app = Flask("Morse")
 api = Api(app)
 
+
+class Main(Resource):
+    def head(self):
+        return "online", 200
+
+    def get(self):
+        return "online", 200
+
+
+api.add_resource(Main, "/")
 api.add_resource(Languages, "/get/json-languages")
 api.add_resource(Files, "/download/json_files")
 api.add_resource(SetupVersion, "/get/setup-version")
